77f61b75a3d06ef1ee8db93fa7db0f7abe4e7140
Rajsubhod <rajdeepmukherjee721@gmail.com>
2025-07-23 20:55:07 +0530

    added server config and parser

---
+++ parser.h
@@ -0,0 +1,19 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+typedef struct {
+    int PORT;
+    int BUFFER_SIZE;
+    int MAX_CLIENTS;
+    char FTP_ROOT[256];
+    char CERT_FILE[256];
+    char KEY_FILE[256];
+    int CLIENT_TIMEOUT;
+    int DATA_TIMEOUT;
+} server_config_t;
+
+server_config_t* load_config(const char* config_file);
+void free_config(server_config_t* config);
+void print_config(const server_config_t* config);
+
+#endif
