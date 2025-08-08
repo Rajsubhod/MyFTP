0689753c52d968b154b92580519defe7b9bbca70
rajdeep <rajdeepmukherjee2002@gmail.com>
2025-08-09 00:48:05 +0530

    ftp command for listing dir contents

--- commands.c
+++ commands.c
@@ -27,6 +27,52 @@ int ssl_recv(ftp_session_t* session, void* data, size_t len)
     return recv(session->control_socket, data, len, 0);
 }
 
+int ssl_data_send(ftp_session_t* session, const void* data, size_t len)
+{
+    if (session->protection_level != FTP_PROT_CLEAR && session->ssl_data_channel)
+    {
+        return SSL_write(session->ssl_data_channel, data, len);
+    }
+    return send(session->data_socket, data, len, 0);
+}
+
+int ssl_data_recv(ftp_session_t* session, void* data, size_t len)
+{
+    if (session->protection_level != FTP_PROT_CLEAR && session->ssl_data_channel)
+    {
+        return SSL_read(session->ssl_data_channel, data, len);
+    }
+    return recv(session->data_socket, data, len, 0);
+}
+
+int create_ssl_data_connection(ftp_session_t* session, int data_socket)
+{
+    if (session->protection_level == FTP_PROT_CLEAR)
+    {
+        return 1;
+    }
+
+    session->ssl_data_channel = SSL_new(session->ctx);
+    if (!session->ssl_data_channel)
+    {
+        ERR_print_errors_fp(stderr);
+        close(data_socket);
+        return -1;
+    }
+    SSL_set_fd(session->ssl_data_channel, data_socket);
+    if (SSL_accept(session->ssl_data_channel) <= 0)
+    {
+        ERR_print_errors_fp(stderr);
+        SSL_free(session->ssl_data_channel);
+        session->ssl_data_channel = NULL;
+        close(data_socket);
+        return -1;
+    }
+
+    printf("SSL/TLS enabled for data channel.\n");
+    return data_socket;
+}
+
 void send_response(ftp_session_t* session, int code, const char* message)
 {
     char response[255];
@@ -216,7 +262,7 @@ void handle_ccc(ftp_session_t* session, char* args)
     }
 
     session->ssl_enabled = 0;
-    printf("Control connection cleared of SSL/TLS\n");
+    log_info("Control connection cleared of SSL/TLS\n");
 }
 
 void handle_type(ftp_session_t* session, char* type)
@@ -284,6 +330,8 @@ void handle_feat(ftp_session_t* session, char* args)
         " STRU\r\n"
         " PWD\r\n"
         " CWD\r\n"
+        " PASV\r\n"
+        " LIST\r\n"
         "211 End\r\n";
     ssl_send(session, response, strlen(response));
 }
@@ -360,11 +408,234 @@ void handle_cwd(ftp_session_t* session, char* path)
     if (stat(full_path, &st) == 0 && S_ISDIR(st.st_mode))
     {
         strcpy(session->current_dir, full_path);
-        printf("%s\n", session->current_dir);
+        log_info("%s\n", session->current_dir);
         send_response(session, 250, "Directory changed.");
     }
     else
     {
         send_response(session, 550, "Directory not found.");
     }
+}
+
+void handle_pasv(ftp_session_t* session, char* args)
+{
+    (void)args;
+    if (session->passive_socket > 0)
+    {
+        close(session->passive_socket);
+        session->passive_socket = -1;
+    }
+
+    session->passive_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (session->passive_socket < 0)
+    {
+        send_response(session, 425, "Can't open passive connection.");
+        return;
+    }
+
+    int opt = 1;
+    if (setsockopt(session->passive_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        perror("setsockopt SO_REUSEADDR failed");
+    }
+
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+    if (getsockname(session->control_socket, (struct sockaddr*)&server_addr, &addr_len) < 0)
+    {
+        perror("getsockname on control socket failed");
+        send_response(session, 425, "Can't get server address.");
+        close(session->passive_socket);
+        session->passive_socket = -1;
+        return;
+    }
+
+    struct sockaddr_in pasv_addr;
+    memset(&pasv_addr, 0, sizeof(pasv_addr));
+    pasv_addr.sin_family = AF_INET;
+    pasv_addr.sin_addr = server_addr.sin_addr;
+    pasv_addr.sin_port = 0;
+
+    if (bind(session->passive_socket, (struct sockaddr*)&pasv_addr, sizeof(pasv_addr)) < 0)
+    {
+        perror("bind failed");
+        send_response(session, 425, "Can't bind passive connection.");
+        close(session->passive_socket);
+        session->passive_socket = -1;
+        return;
+    }
+
+    if (listen(session->passive_socket, 1) < 0)
+    {
+        perror("listen failed");
+        send_response(session, 425, "Can't listen on passive connection.");
+        close(session->passive_socket);
+        session->passive_socket = -1;
+        return;
+    }
+
+    addr_len = sizeof(pasv_addr);
+    if (getsockname(session->passive_socket, (struct sockaddr*)&pasv_addr, &addr_len) < 0)
+    {
+        perror("getsockname failed");
+        send_response(session, 425, "Can't get passive connection info.");
+        close(session->passive_socket);
+        session->passive_socket = -1;
+        return;
+    }
+    session->passive_mode = 1;
+    session->data_port = ntohs(pasv_addr.sin_port);
+
+    uint32_t ip = ntohl(pasv_addr.sin_addr.s_addr);
+    char response[2048];
+    snprintf(response, sizeof(response), "Entering Passive Mode (%d,%d,%d,%d,%d,%d)",
+             (ip >> 24) & 0xFF, (ip >> 16) & 0xFF,
+             (ip >> 8) & 0xFF, ip & 0xFF,
+             session->data_port / 256,
+             session->data_port % 256);
+
+    printf("PASV: Listening on %s:%d\n", inet_ntoa(pasv_addr.sin_addr), session->data_port);
+    send_response(session, 227, response);
+
+    log_info("PASV: Response sent, waiting for client command...\n");
+}
+
+void handle_list(ftp_session_t* session, char* args)
+{
+    (void)args;
+    if (!session->is_authenticated)
+    {
+        send_response(session, 530, "Not logged in.");
+        return;
+    }
+
+    char full_path[512];
+    snprintf(full_path, sizeof(full_path), "%s", session->current_dir);
+
+    // printf("%s\n", full_path);
+
+    DIR* dir = opendir(full_path);
+    if (!dir)
+    {
+        send_response(session, 550, "Failed to open directory.");
+        return;
+    }
+
+    send_response(session, 150, "Opening data connection for directory listing.");
+
+    printf("LIST: Waiting for data connection on passive socket %d...\n", session->passive_socket);
+
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    setsockopt(session->passive_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+
+    int data_sock = accept(session->passive_socket, NULL, NULL);
+    if (data_sock < 0)
+    {
+        perror("accept failed for LIST");
+        send_response(session, 425, "Can't open data connection.");
+        closedir(dir);
+        return;
+    }
+
+    // printf("LIST: Data connection established successfully!\n");
+
+    session->data_socket = data_sock;
+    if (create_ssl_data_connection(session, data_sock) < 0)
+    {
+        send_response(session, 425, "Can't open data connection.");
+        closedir(dir);
+        return;
+    }
+
+    struct dirent* entry;
+    char list_item[1024];
+
+    time_t now = time(NULL);
+    struct tm* now_tm = localtime(&now);
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+
+        char item_path[1024];
+        size_t path_len = strlen(full_path);
+        size_t name_len = strlen(entry->d_name);
+
+        if (path_len + 1 + name_len >= sizeof(item_path))
+        {
+            fprintf(stderr, "Skipping too-long path: %s/%s\n", full_path, entry->d_name);
+            continue;
+        }
+        snprintf(item_path, sizeof(item_path), "%s/%s", full_path, entry->d_name);
+
+        struct stat st;
+        if (stat(item_path, &st) == 0)
+        {
+            char permissions[11];
+            snprintf(permissions, sizeof(permissions), "%c%c%c%c%c%c%c%c%c%c",
+                     (S_ISDIR(st.st_mode) ? 'd' : '-'),
+                     (st.st_mode & S_IRUSR) ? 'r' : '-',
+                     (st.st_mode & S_IWUSR) ? 'w' : '-',
+                     (st.st_mode & S_IXUSR) ? 'x' : '-',
+                     (st.st_mode & S_IRGRP) ? 'r' : '-',
+                     (st.st_mode & S_IWGRP) ? 'w' : '-',
+                     (st.st_mode & S_IXGRP) ? 'x' : '-',
+                     (st.st_mode & S_IROTH) ? 'r' : '-',
+                     (st.st_mode & S_IWOTH) ? 'w' : '-',
+                     (st.st_mode & S_IXOTH) ? 'x' : '-');
+
+            char time_str[32];
+
+            time_t file_time = st.st_mtime;
+            struct tm* tm_info = localtime(&file_time);
+
+            if (tm_info == NULL) {
+                snprintf(time_str, sizeof(time_str), "Jan  1  1970");
+            } else {
+                double time_diff = difftime(now, file_time);
+
+                // If file is less than 6 months old, show time; otherwise show year
+                if (time_diff >= 0 && time_diff < (6 * 30 * 24 * 3600)) { // ~6 months in seconds
+                    strftime(time_str, sizeof(time_str), "%b %d %H:%M", tm_info);
+                } else {
+                    strftime(time_str, sizeof(time_str), "%b %d  %Y", tm_info);
+                }
+            }
+
+            snprintf(list_item, sizeof(list_item),
+                     "%s   1 ftp      ftp      %8ld %s %s\r\n",
+                     permissions, (long)st.st_size, time_str, entry->d_name);
+
+            if (ssl_data_send(session, list_item, strlen(list_item)) < 0)
+            {
+                perror("send failed");
+                break;
+            }
+        }
+    }
+
+    closedir(dir);
+
+    if (session->ssl_data_channel)
+    {
+        SSL_shutdown(session->ssl_data_channel);
+        SSL_free(session->ssl_data_channel);
+        session->ssl_data_channel = NULL;
+    }
+
+    close(data_sock);
+
+    if (session->passive_socket > 0)
+    {
+        close(session->passive_socket);
+        session->passive_socket = -1;
+    }
+    session->passive_mode = 0;
+
+    send_response(session, 226, "Transfer complete.");
 }
